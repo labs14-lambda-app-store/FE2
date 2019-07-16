ceccdd91d3850addace1a3990fc5d4526087c3d8
joshua-disney <joshua.a.disney@gmail.com>
2019-07-16 15:39:13 -0700

    Begin building project form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+
 import Projects from "./components/Projects";
+import ProjectForm from "./components/ProjectForm";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Lambda App Store!</h1>
-      <Projects />
+      <div className="banana">
+        <Projects />
+        <ProjectForm />
+      </div>
     </div>
   );
 }
