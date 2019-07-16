57261630b3ad55add8e4427be9b30a36bae2b48a
joshua-disney <joshua.a.disney@gmail.com>
2019-07-16 12:41:19 -0700

    Add dependencies, create initial testing suite

---
+++ frontend/src/__tests__/App.specs.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import "@testing-library/react/cleanup-after-each";
+import { render } from "@testing-library/react";
+import App from "../App";
+
+describe("<App />", () => {
+  it("should render the App component without errors", () => {
+    render(<App />);
+  });
+});
