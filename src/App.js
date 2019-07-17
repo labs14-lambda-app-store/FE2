13f3d153818391ca25a8ce9e9a6bad8bb7787bd1
Jeff Lapp <lappjeff20@gmail.com>
2019-07-17 19:35:46 -0400

    Refactored App to be a class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import ProjectForm from "./components/ProjectForm";
+import {Callback, AuthButton} from './components/Auth'
+import { Route } from 'react-router-dom'
 
 import auth0Client from './components/Auth/Auth'
 import "./App.scss";
 
+
 require("dotenv").config();
 
-function App() {
+const App = () => {
+
+  useEffect(() => {
+    console.log(auth0Client.isAuthenticated())
+    console.log(auth0Client.getProfile())
+  }, [])
+  
   return (
     <Router>
       <div className="App">
         <h1>Lambda App Store!</h1>
+        <AuthButton />
         <div className="banana">
           <Projects />
           <ProjectForm />
           <Footer />
         </div>
+
+        <Route exact path='/callback' component={Callback} />
       </div>
     </Router>
   );
