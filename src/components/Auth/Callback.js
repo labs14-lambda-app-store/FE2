ee3f11e1b60812a6f15deee0e85c7894b6e42536
Jeff Lapp <lappjeff20@gmail.com>
2019-07-18 18:55:59 -0400

    Cleared code from callback

--- src/components/Auth/Callback.js
+++ src/components/Auth/Callback.js
@@ -1,21 +1,7 @@
 import React, {useState, useEffect} from "react"
 import {withRouter} from 'react-router-dom';
-import auth0Client from './Auth';
 
 const Callback = props => {
-
-
-  async function handleAuthentication() {
-    await auth0Client.handleAuthentication();
-    props.history.replace('/')
-
-  }
-
-  useEffect(() => {
-    handleAuthentication()
-
-  }, [])
-
 
   return (
     <p>Loading profile...</p>
