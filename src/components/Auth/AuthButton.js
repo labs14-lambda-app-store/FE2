8b8f55c7ece56ec4351844dc9315ad1cba7d96f4
Jeff Lapp <lappjeff20@gmail.com>
2019-07-19 11:39:15 -0400

    Created logoutWithRedirect function to log out and redirect to origin url

--- src/components/Auth/AuthButton.js
+++ src/components/Auth/AuthButton.js
@@ -1,9 +1,37 @@
 import React, {useState, useEffect} from "react"
+import { useAuth0 } from './react-auth0-spa.js'
 
 const AuthButton = props => {
 
+  const { isAuthenticated, loginWithRedirect, loginWithPopup, logout, user} = useAuth0()
 
-  return null
+  const logoutWithRedirect = () => {
+    logout({
+      returnTo: window.location.origin
+    })
+  }
+
+  return (
+    <>
+    {
+      !isAuthenticated && (
+        <>
+        <button onClick={() => loginWithRedirect({ redirect_uri: 'http://localhost:3000/callback'})}> Sign In </button>
+        {console.log(user)}
+        </>
+      )
+    }
+    {
+      isAuthenticated && (
+        <>
+        <button onClick={() => logoutWithRedirect()}> Sign out </button>
+        <img src={user.picture} alt="Profile"/>
+        </>
+      )
+
+    }
+    </>
+  )
 }
 
  export default AuthButton
