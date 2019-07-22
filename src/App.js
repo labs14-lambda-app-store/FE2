952777ed0b82f4dec402431ba9d2eaa2ba50524f
chrispetito <christopherpetito@gmail.com>
2019-07-22 14:40:35 -0700

    render projects

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import ProjectForm from "./components/ProjectForm";
-import {Callback, AuthButton} from './components/Auth'
-import { Route } from 'react-router-dom'
-import { useAuth0 } from './components/Auth/react-auth0-spa'
+import { Callback, AuthButton } from "./components/Auth";
+import { Route } from "react-router-dom";
+import { useAuth0 } from "./components/Auth/react-auth0-spa";
 import "./App.scss";
 
 require("dotenv").config();
 
-
 const App = () => {
-  const { loading } = useAuth0()
+  const { loading } = useAuth0();
 
-  if(loading) return "Loading..."
+  if (loading) return "Loading...";
   return (
     <Router>
       <div className="App">
         <h1>Lambda App Store!</h1>
+
         <AuthButton />
         <div className="banana">
           <Projects />
@@ -27,10 +27,10 @@ const App = () => {
           <Footer />
         </div>
 
-        <Route exact path='/callback' component={Callback} />
+        <Route exact path="/callback" component={Callback} />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,24 +8,33 @@ const Projects = () => {
 
   useEffect(() => {
     isMounted = true;
+    console.log(isMounted)
     axios
       .get(`https://lambdaappstore2.herokuapp.com/api/projects`)
       .then(res => {
-        if (isMounted) {
+        console.log(res)
+        if (isMounted === true) {
+          console.log('Mounted triggering')
+
           setProjects(res.data);
+        } else {
+          console.log('ELSE')
         }
       })
       .catch(err => console.log(err.message));
-    isMounted = false;
+    // isMounted = false;
   }, []);
 
   return (
     <div>
+      <h1>Hi</h1>
       {projects.map(project => (
-        <div>
+        <div key={project.id}>
+          {console.log(project)}
+          <h1>Projects Test</h1>
           <p>{project.id}</p>
           <h1>{project.name}</h1>
-          <h3>{project.url}</h3>
+          <h3>{project.hosted_url}</h3>
           <h3>{project.description}</h3>
         </div>
       ))}
