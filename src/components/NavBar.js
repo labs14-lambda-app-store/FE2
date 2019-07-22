698cecaae4af4da87d68fa941de9b05ca9784295
Shadowborn <james.starks023@gmail.com>
2019-07-22 16:34:10 -0600

    code comments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ import { MemoryRouter as Router } from 'react-router-dom';
 import { BrowserRouter as Route, Link, withRouter } from "react-router-dom";
 
 // I've imported Material UI packages above, 
-// and set them in a functional component below
+// and implemented them in a functional component below
 const NavBar = (props) => {
 
     return (
@@ -16,6 +16,8 @@ const NavBar = (props) => {
                 {console.log(props)}
                 <AppBar position="static">
                     <Toolbar>
+                        {/* Using button onClick to avoid Link bug that prevents route changes, 
+                        when using material-ui */}
                         <Button onClick={() => props.history.push('/')}>Home</Button>
                         <Button onClick={() => props.history.push('/projects')}>Projects</Button>
                         <Button onClick={() => props.history.push('/project-form')}>Submit Project</Button>
