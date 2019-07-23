85e976c25c8dce2e7b630fac36b65dbb9ceb34d2
Jeff Lapp <lappjeff20@gmail.com>
2019-07-23 17:35:59 -0400

    - Imported action types from actions - Created switch cases for all action types

---
+++ src/reducers/projectsReducer.js
@@ -0,0 +1,33 @@
+import {
+  GET_PROJECTS_START,
+  GET_PROJECTS_SUCCESS,
+  GET_PROJECTS_FAIL
+} from '../actions'
+
+const initialState = {
+  projects: [],
+  isFetching: false,
+  isAdding: true
+}
+
+const projectsReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case GET_PROJECTS_START:
+      return {
+        ...state,
+        isFetching: true
+      }
+    case GET_PROJECTS_SUCCESS:
+      return {
+        ..state,
+        projects: action.payload,
+        isFetching: false
+      }
+    case GET_PROJECTS_FAIL:
+      return {
+        ...state,
+        isFetching: false
+      }
+    default: return state
+  }
+}
