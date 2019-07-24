22529283fca162844f9e7e1af237959bfbd84658
Kayla <6lackmacaroon@gmail.com>
2019-07-24 14:08:08 -0600

    netlify error abatement

--- src/components/SubmissionForm/Confirm.js
+++ src/components/SubmissionForm/Confirm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
