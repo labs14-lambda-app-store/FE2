a2eb72df11381650874c8636cfd5637d96c55641
Jeff Lapp <lappjeff20@gmail.com>
2019-07-24 11:47:24 -0400

    Refactor to async/await
    - Refactored projectActions to use async/await out of desperation
    - WIP

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -4,15 +4,25 @@ export const GET_PROJECTS_START = 'GET_PROJECTS_START'
 export const GET_PROJECTS_SUCCESS = 'GET_PROJECTS_SUCCESS'
 export const GET_PROJECTS_FAIL = 'GET_PROJECTS_FAIL'
 
-export const getProjects = () => dispatch => {
+export const getProjects = () => async dispatch => {
   dispatch({ type: GET_PROJECTS_START})
 
-  return axios
-    .get(`https://lambdaappstore2.herokuapp.com/api/projects`)
-    .then(res => {
-      dispatch({ type: GET_PROJECTS_SUCCESS, payload: res.data})
-    })
-    .catch(err => {
-      dispatch({ type: GET_PROJECTS_FAIL, payload: err})
-    })
+
+  try {
+    const result = await axios.get(`https://lambdaappstore2.herokuapp.com/api/projects`)
+    dispatch({ type: GET_PROJECTS_SUCCESS, payload: result.data})
+  } catch(err) {
+    dispatch({ type: GET_PROJECTS_FAIL, payload: err})
+  }
+
+
+  // // console.log(result.data)
+  // return axios
+  //   .get(`https://lambdaappstore2.herokuapp.com/api/projects`)
+  //   .then(res => {
+  //     dispatch({ type: GET_PROJECTS_SUCCESS, payload: res.data})
+  //   })
+  //   .catch(err => {
+  //     dispatch({ type: GET_PROJECTS_FAIL, payload: err})
+  //   })
 }
