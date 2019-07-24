09eae997a9a96f011b1ae51b52f0f6b5a9bdd380
Jeff Lapp <lappjeff20@gmail.com>
2019-07-24 13:12:45 -0400

    Added create project action types and function to index.js exports

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,5 +2,9 @@ export {
   GET_PROJECTS_START,
   GET_PROJECTS_SUCCESS,
   GET_PROJECTS_FAIL,
-  getProjects
+  ADD_PROJECTS_START,
+  ADD_PROJECTS_SUCCESS,
+  ADD_PROJECTS_FAIL,
+  getProjects,
+  addProject
 } from './projectActions'
