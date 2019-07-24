4e330718804fd2b3255c6fbb9f72d93456bc7b79
Jeff Lapp <lappjeff20@gmail.com>
2019-07-24 17:13:04 -0400

    Removed _blank targets
    - Changed all _blank targets to # to avoid console warnings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,26 +7,26 @@ export default function Footer() {
 		<>
 			<footer>
 				<nav>
-					<a href="https://lambdaschool.com/courses/cs/web/" target="_blank">COURSES</a>
-					<a href="https://lambdaschool.com/about/" target="_blank">ABOUT</a>
-					<a href="https://lambdaschool.com/blog/" target="_blank">BLOG</a>
-					<a href="https://lambdaschool.com/apply/" target="_blank">APPLY</a>
-					<a href="https://lambdaschool.com/careers/" target="_blank">CAREERS</a>
+					<a href="https://lambdaschool.com/courses/cs/web/" target="#" >COURSES</a>
+					<a href="https://lambdaschool.com/about/" target="#">ABOUT</a>
+					<a href="https://lambdaschool.com/blog/" target="#">BLOG</a>
+					<a href="https://lambdaschool.com/apply/" target="#">APPLY</a>
+					<a href="https://lambdaschool.com/careers/" target="#">CAREERS</a>
 					<Link to="/privacy">PRIVACY</Link>
 					<Link to="/terms">TERMS</Link>
-					<a href="https://lambdaschool.com/contact/" target="_blank">CONTACT</a>
-					<a href="https://lambdaschool.com/courses/cs/web/bootcamp/" target="_blank">
+					<a href="https://lambdaschool.com/contact/" target="#">CONTACT</a>
+					<a href="https://lambdaschool.com/courses/cs/web/bootcamp/" target="#">
 						FREE CODE BOOTCAMP
 					</a>
-					<a href="https://lambdaschool.com/referral/" target="_blank">REFERRAL</a>
+					<a href="https://lambdaschool.com/referral/" target="#">REFERRAL</a>
 					<div className="sm-links-mobile">
-						<a href="https://twitter.com/lambdaschool" target="_blank">
+						<a href="https://twitter.com/lambdaschool" target="#">
 							<i className="fab fa-twitter" />
 						</a>
-						<a href="https://www.facebook.com/LambdaSchoolOnline/" target="_blank">
+						<a href="https://www.facebook.com/LambdaSchoolOnline/" target="#">
 							<i className="fab fa-facebook" />
 						</a>
-						<a href="https://github.com/LambdaSchool" target="_blank">
+						<a href="https://github.com/LambdaSchool" target="#">
 							<i className="fab fa-github" />
 						</a>
 					</div>
