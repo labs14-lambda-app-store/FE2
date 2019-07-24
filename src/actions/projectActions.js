bfea1c0e3a9400fee6571a54215bdf305b9ee705
Jeff Lapp <lappjeff20@gmail.com>
2019-07-24 12:56:34 -0400

    - Created async/await function for creating a new project - Implemented try/catch block

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -14,12 +14,20 @@ export const getProjects = () => async dispatch => {
   } catch(err) {
     dispatch({ type: GET_PROJECTS_FAIL, payload: err})
   }
+}
 
 export const ADD_PROJECTS_START = 'ADD_PROJECTS_START'
 export const ADD_PROJECTS_SUCCESS = 'ADD_PROJECTS_SUCCESS'
 export const ADD_PROJECTS_FAIL = 'ADD_PROJECTS_FAIL'
 
-export const addProject = () => async dispatch => {
-  dispatch({})
-}
+export const addProject = newProject => async dispatch => {
+  dispatch({ type: ADD_PROJECTS_START})
+
+  try {
+    const postResult = await axios.post(`https://lambdaappstore2.herokuapp.com/api/projects`, newProject)
+    dispatch({ type: ADD_PROJECTS_SUCCESS, payload: postResult})
+  } catch(err) {
+    dispatch({ type: ADD_PROJECTS_FAIL, payload: err})
+  }
+  
 }
