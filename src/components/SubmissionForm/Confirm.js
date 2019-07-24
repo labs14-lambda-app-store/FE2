54f1c7d029d73a04bc468914d9e7146c143abce5
Kayla Crow <44589691+blackmacaroon@users.noreply.github.com>
2019-07-24 13:58:20 -0600

    Update Confirm.js
    removed unused imports

--- src/components/SubmissionForm/Confirm.js
+++ src/components/SubmissionForm/Confirm.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
