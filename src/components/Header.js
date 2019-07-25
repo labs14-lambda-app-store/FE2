4b8b7d3195cdc2485c1c91eba43bd7cebb9638b8
chrispetito <christopherpetito@gmail.com>
2019-07-25 10:15:18 -0700

    add student img to header, lato font

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import lambdaStudent from '../assets/lambda-student.png'
 
 export default function Header() {
   return (
     <header>
       <div className="home-header">
         <p>Lambda App Store</p>
+        <img src={lambdaStudent}></img>
       </div>
     </header>
   );
