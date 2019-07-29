ccd7f25cb43bcb64db79468a7f4504bac3ad84bf
Jeff Lapp <lappjeff20@gmail.com>
2019-07-29 19:58:47 -0400

    Created Image uploading functionality

--- src/components/SubmissionForm/ImageUpload.js
+++ src/components/SubmissionForm/ImageUpload.js
@@ -1,10 +1,36 @@
 import React, { useState } from "react"
 import axios from "axios"
+
+const cloudinaryUrl = process.env.REACT_APP_CLOUDINARY_URL
+const uploadPreset = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
+
 const ImageUpload = props => {
-// })
+  const [imgPreviewUrl, setImgPreviewUrl] = useState()
+
+  const handleImage = async e => {
+    let file = e.target.files[0]
+    let formData = new FormData()
+
+    formData.append("file", file)
+    formData.append("upload_preset", uploadPreset)
+
+    axios
+      .post(cloudinaryUrl, formData, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      })
+      .then(res => {
+        setImgPreviewUrl(res.data.secure_url)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
     <div className="card">
-      <img src={imgPreviewUrl} id="img-preview" />
+      <img src={imgPreviewUrl} id="img-preview" alt="Chosen file preview" />
       <label className="file-upload-container" htmlFor="file-upload">
         <input
           id="file-upload"
