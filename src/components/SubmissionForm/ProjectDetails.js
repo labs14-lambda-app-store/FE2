6e7549104a2e7981e34ce2c9b342efda0b93c9a1
Kayla <6lackmacaroon@gmail.com>
2019-07-29 14:22:37 -0600

    testing

--- src/components/SubmissionForm/ProjectDetails.js
+++ src/components/SubmissionForm/ProjectDetails.js
@@ -7,6 +7,7 @@ import Button from "@material-ui/core/Button"
 
 const ProjectDetails = props => {
   const { nextStep, state, handleStateChanges } = props
+  console.log(state)
   const {
     hosted_url,
     frontend_url,
