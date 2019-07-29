3a435b9c4ff428a570347f905fc54d4636d2edb1
Jeff Lapp <lappjeff20@gmail.com>
2019-07-29 19:44:09 -0400

    Created html layout

--- src/components/SubmissionForm/ImageUpload.js
+++ src/components/SubmissionForm/ImageUpload.js
@@ -2,5 +2,20 @@ import React, { useState } from "react"
 import axios from "axios"
 const ImageUpload = props => {
 // })
+  return (
+    <div className="card">
+      <img src={imgPreviewUrl} id="img-preview" />
+      <label className="file-upload-container" htmlFor="file-upload">
+        <input
+          id="file-upload"
+          onChange={e => handleImage(e)}
+          type="file"
+          style={{ display: "none" }}
+        />
+        Select an Image
+      </label>
+    </div>
+  )
+}
 
 export default ImageUpload
