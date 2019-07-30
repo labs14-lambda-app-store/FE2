2d7903b74b431756755387dc11087950b0c22396
Jeff Lapp <lappjeff20@gmail.com>
2019-07-29 20:02:08 -0400

    Created add image action types

--- src/actions/imageActions.js
+++ src/actions/imageActions.js
@@ -1,1 +1,5 @@
 import axios from "axios"
+
+export const POST_IMAGE_START = "POST_IMAGE_START"
+export const POST_IMAGE_FAIL = "POST_IMAGE_FAIL"
+export const POST_IMAGE_SUCCESS = "POST_IMAGE_SUCCESS"
