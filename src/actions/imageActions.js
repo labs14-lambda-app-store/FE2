2122bf3042df25fa3aa62a01989e00193b004c57
Jeff Lapp <lappjeff20@gmail.com>
2019-07-29 20:08:39 -0400

    Created try/catch block to handle image posting

--- src/actions/imageActions.js
+++ src/actions/imageActions.js
@@ -8,11 +8,22 @@ const cloudinaryUrl = process.env.REACT_APP_CLOUDINARY_URL
 const uploadPreset = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
 
 export const addImage = e => async dispatch => {
-  dispatch({type: POST_IMAGE_START})
+  dispatch({ type: POST_IMAGE_START })
 
   let file = e.target.files[0]
   let formData = new FormData()
 
   formData.append("file", file)
   formData.append("upload_preset", uploadPreset)
+
+  try {
+    const image = await axios.post(cloudinaryUrl, formData, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+    dispatch({ type: POST_IMAGE_SUCCESS, payload: image })
+  } catch (err) {
+    dispatch({ type: POST_IMAGE_FAIL, payload: err })
+  }
 }
