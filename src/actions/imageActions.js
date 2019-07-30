33d5d30e7b0202ea8fe63266e86996d95f970efd
Jeff Lapp <lappjeff20@gmail.com>
2019-07-29 20:05:24 -0400

    Created and added necessary variables for upload

--- src/actions/imageActions.js
+++ src/actions/imageActions.js
@@ -3,3 +3,16 @@ import axios from "axios"
 export const POST_IMAGE_START = "POST_IMAGE_START"
 export const POST_IMAGE_FAIL = "POST_IMAGE_FAIL"
 export const POST_IMAGE_SUCCESS = "POST_IMAGE_SUCCESS"
+
+const cloudinaryUrl = process.env.REACT_APP_CLOUDINARY_URL
+const uploadPreset = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
+
+export const addImage = e => async dispatch => {
+  dispatch({type: POST_IMAGE_START})
+
+  let file = e.target.files[0]
+  let formData = new FormData()
+
+  formData.append("file", file)
+  formData.append("upload_preset", uploadPreset)
+}
