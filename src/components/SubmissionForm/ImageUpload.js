410aaf0c1aab069b06874b2b10bf6e35c0055377
Jeff Lapp <lappjeff20@gmail.com>
2019-07-29 20:32:56 -0400

    Integrated Redux into ImageUpload
    - Removed logic and functionality from file
    - Implemented Redux

--- src/components/SubmissionForm/ImageUpload.js
+++ src/components/SubmissionForm/ImageUpload.js
@@ -1,40 +1,18 @@
 import React, { useState } from "react"
-import axios from "axios"
+import { connect } from "react-redux"
+import { sendImageToCloudinary } from "../../actions"
 
 const cloudinaryUrl = process.env.REACT_APP_CLOUDINARY_URL
 const uploadPreset = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
 
 const ImageUpload = props => {
-  const [imgPreviewUrl, setImgPreviewUrl] = useState()
-
-  const handleImage = async e => {
-    let file = e.target.files[0]
-    let formData = new FormData()
-
-    formData.append("file", file)
-    formData.append("upload_preset", uploadPreset)
-
-    axios
-      .post(cloudinaryUrl, formData, {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      })
-      .then(res => {
-        setImgPreviewUrl(res.data.secure_url)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-
   return (
     <div className="card">
-      <img src={imgPreviewUrl} id="img-preview" alt="Chosen file preview" />
+      <img src={""} id="img-preview" alt="Chosen file preview" />
       <label className="file-upload-container" htmlFor="file-upload">
         <input
           id="file-upload"
-          onChange={e => handleImage(e)}
+          onChange={e => props.sendImageToCloudinary(e)}
           type="file"
           style={{ display: "none" }}
         />
@@ -44,4 +22,13 @@ const ImageUpload = props => {
   )
 }
 
-export default ImageUpload
+const mapStateToProps = ({ imagesReducer }) => {
+  return {
+    ...imagesReducer,
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { sendImageToCloudinary }
+)(ImageUpload)
