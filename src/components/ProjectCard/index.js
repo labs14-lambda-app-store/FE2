62fbefb41f46fa90d292796b76d60e742a516e5f
Jeff Lapp <lappjeff20@gmail.com>
2019-07-31 17:25:06 -0400

    Added exports to index

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,4 +1,6 @@
 import Project from "./Project"
 import ProjectSearch from "./ProjectSearch"
+import ProjectModal from "./ProjectModal"
+import ModalContent from "./ModalContent"
 
-export { Project, ProjectSearch }
+export { Project, ProjectSearch, ProjectModal, ModalContent }
