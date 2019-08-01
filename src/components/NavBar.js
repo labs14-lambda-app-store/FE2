b9530ec6eb53a04888147608452d327898ece6df
Kayla Crow <44589691+blackmacaroon@users.noreply.github.com>
2019-08-01 13:51:08 -0600

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,4 @@
 import React from "react"
-import {connect} from 'react-redux';
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Button from "@material-ui/core/Button"
