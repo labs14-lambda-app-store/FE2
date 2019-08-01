47f584062b4f2c4553cdfad1d4a5e4aa1f6873ed
Jeff Lapp <lappjeff20@gmail.com>
2019-07-31 20:06:51 -0400

    Set up basic layout for ProjectCard

--- src/components/ProjectCard/ModalContent.js
+++ src/components/ProjectCard/ModalContent.js
@@ -1,7 +1,52 @@
 import React from "react"
+import Chip from "@material-ui/core/Chip"
+import Avatar from "@material-ui/core/Avatar"
 
+import "../../styles/modal.scss"
 const ModalContent = props => {
-  return <h1>Modal content</h1>
+  const { project } = props
+  return (
+    <main className="modal-content">
+      <h2 className="name">{project.name}</h2>
+      <p className="description">{project.description}</p>
+      <div className="github">
+        <h4>
+          Github <i className="fab fa-github"></i>
+        </h4>
+        <ul>
+          <li>
+            <a href={project.backend_url} target={"_blank"}>
+              Backend
+            </a>
+          </li>
+          <li>
+            <a href={project.frontend_url} target={"_blank"}>
+              Frontend
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="tags">
+        {project.tags &&
+          project.tags.map(tag => {
+            return (
+              <Chip
+                variant="outlined"
+                color="secondary"
+                key={tag.tag_name}
+                label={tag.tag_name}
+                style={{ margin: "2px" }}
+                avatar={
+                  <Avatar>
+                    <i className="fas fa-tag"></i>
+                  </Avatar>
+                }
+              />
+            )
+          })}
+      </div>
+    </main>
+  )
 }
 
 export default ModalContent
