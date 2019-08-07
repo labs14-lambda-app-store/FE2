7210ab77a006ea865ae6bcbb0aea5462b7952aea
Kayla <6lackmacaroon@gmail.com>
2019-08-07 14:09:24 -0600

    creat basic custom palette for styling - test by removing a few inline color styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from "react"
+import { useAuth0 } from "./components/Auth/react-auth0-spa"
 import { BrowserRouter as Router } from "react-router-dom"
+import { createMuiTheme } from "@material-ui/core/styles"
+import { ThemeProvider } from "@material-ui/styles"
+import { Route } from "react-router-dom"
 
-import Home from "./components/Home"
+import ProjectForm from "./components/SubmissionForm/ProjectForm"
+import PendingSearch from "./components/Admin/PendingSearch"
+import { ProjectSearch } from "./components/ProjectCard"
 import NavBar from "./components/NavBar"
 import Footer from "./components/Footer"
-import ProjectForm from "./components/SubmissionForm/ProjectForm"
-import { ProjectSearch } from "./components/ProjectCard"
-import { Route } from "react-router-dom"
-import { useAuth0 } from "./components/Auth/react-auth0-spa"
+import Header from "./components/Header"
+import Home from "./components/Home"
 
 import "./App.scss"
-import Header from "./components/Header"
-import PendingSearch from "./components/Admin/PendingSearch";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1a61b0',
+      contrastText: '#efeef3',
+    },
+    secondary: {
+      main: '#f32667',
+      contrastText: '#efeef3',
+    }
+  }
+})
 
 const App = () => {
   const { loading } = useAuth0()
@@ -19,16 +34,18 @@ const App = () => {
   if (loading) return null
   return (
     <Router>
-      <div className="App">
-        <NavBar />
-        <Header />
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <NavBar />
+          <Header />
 
-        <Route exact path="/" component={Home} />
-        <Route exact path="/project-form" component={ProjectForm} />
-        <Route path="/projects" exact component={ProjectSearch} />
-        <Route path="/pending-projects" exact component={PendingSearch} />
-        <Footer />
-      </div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/project-form" component={ProjectForm} />
+          <Route path="/projects" exact component={ProjectSearch} />
+          <Route path="/pending-projects" exact component={PendingSearch} />
+          <Footer />
+        </div>
+      </ThemeProvider>
     </Router>
   )
 }
