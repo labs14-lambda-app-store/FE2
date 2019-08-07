d2fcff3c102b5c1ca878703ebf9f2a1afec20d5a
Kayla <6lackmacaroon@gmail.com>
2019-08-07 15:02:33 -0600

    removed pending modal style - there is no more pending modal

--- src/components/reusable/AdminButtons.js
+++ src/components/reusable/AdminButtons.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Button from "@material-ui/core/Button"
 
+const style ={fontSize: "1.4rem", margin: "0 10px"}
+
 const AdminButtons = props => {
   return (
     <div className="admin-buttons">
@@ -8,6 +10,7 @@ const AdminButtons = props => {
         className="admin-button"
         size="small"
         color="primary"
+        style={style}
         onClick={() => console.log("ello govna")}
       >
         Approve
@@ -16,6 +19,7 @@ const AdminButtons = props => {
         className="admin-button"
         size="small"
         color="secondary"
+        style={style}
         onClick={() => console.log("sorry govna")}
       >
         Deny
