2119a39a950eef6ae7edb4b3186eaf66b2c051cc
victorN <victornguyen082@gmail.com>
2019-08-08 13:23:41 -0400

    approve dialog created,
    Co-authored-by: James <15709701+Shadowborn@users.noreply.github.com>
    Co-authored-by: Kayla <44589691+blackmacaroon@users.noreply.github.com>
    Co-authored-by: Chris <48688296+chrispetito@users.noreply.github.com>

--- src/components/reusable/AdminButtons.js
+++ src/components/reusable/AdminButtons.js
@@ -1,9 +1,24 @@
 import React from "react"
 import Button from "@material-ui/core/Button"
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const style ={fontSize: "1.4rem", margin: "0 10px"}
 
 const AdminButtons = props => {
+  const [open, setOpen] = React.useState(false);
+
+  function handleClickOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
   return (
     <div className="admin-buttons">
       <Button
@@ -11,7 +26,7 @@ const AdminButtons = props => {
         size="small"
         color="primary"
         style={style}
-        onClick={() => console.log("ello govna")}
+        onClick={handleClickOpen}
       >
         Approve
       </Button>
@@ -24,6 +39,28 @@ const AdminButtons = props => {
       >
         Deny
       </Button>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Approve this project?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to approve this project? Approving this project will move it into the gallery.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Approve
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   )
 }
