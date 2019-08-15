fd801bcf702174f2097c8517c638d95d60a6477c
joshua-disney <joshua.a.disney@gmail.com>
2019-08-15 09:53:26 -0700

    Create coming soon stamp

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,14 @@ import lambdaLandingUX from "../assets/LambdaLandingUX.png"
 
 const Home = () => {
   return (
-    <img
-      className="landingMockup"
-      src={lambdaLandingUX}
-      alt="lambda landing page mockup"
-    />
+    <div landing-page>
+      <img
+        className="landingMockup"
+        src={lambdaLandingUX}
+        alt="lambda landing page mockup"
+      />
+      <div className="landing-page coming-soon">COMING SOON!</div>
+    </div>
   )
 }
 
