29920aea9330641b83726a2927a4514f413da9eb
Jeff Lapp <lappjeff20@gmail.com>
2019-08-19 12:32:31 -0400

    Added comments to AuthButton

--- src/components/Auth/AuthButton.js
+++ src/components/Auth/AuthButton.js
@@ -18,6 +18,7 @@ const AuthButton = ({ first_name }) => {
 
   return (
     <>
+      //displays sign in button if no user cookie found
       {!userIDCookie && (
         <>
           <Button onClick={() => loginWithRedirect({})} color="secondary">
@@ -25,6 +26,7 @@ const AuthButton = ({ first_name }) => {
           </Button>
         </>
       )}
+      //displays sign out button if user cookie found
       {userIDCookie && (
         <>
           {first_name && (
