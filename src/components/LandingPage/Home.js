21f32d629e939ade48a386f60e892cc7dfd78530
Jeff Lapp <lappjeff20@gmail.com>
2019-08-19 12:30:15 -0400

    Changed name of image import to be more descriptive

--- src/components/LandingPage/Home.js
+++ src/components/LandingPage/Home.js
@@ -1,12 +1,12 @@
 import React from "react"
-import uxPlaceholder from "../../assets/LambdaLandingUX.png"
+import uxPlaceholderImg from "../../assets/LambdaLandingUX.png"
 
 const Home = () => {
   return (
     <div className="landing-page">
       <img
         className="landingMockup"
-        src={uxPlaceholder}
+        src={uxPlaceholderImg}
         alt="lambda landing page mockup"
       />
       <div className="coming-soon">COMING SOON!</div>
