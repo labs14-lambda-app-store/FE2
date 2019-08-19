43976b6f02be3221c5b10911ff1759af8468034b
Jeff Lapp <lappjeff20@gmail.com>
2019-08-19 12:08:19 -0400

    Changed import name of ux mockup to prevent confusion with React component

--- src/components/LandingPage/Home.js
+++ src/components/LandingPage/Home.js
@@ -1,12 +1,12 @@
 import React from "react"
-import LambdaLandingUX from "../../assets/LambdaLandingUX.png"
+import uxPlaceholder from "../../assets/LambdaLandingUX.png"
 
 const Home = () => {
   return (
     <div className="landing-page">
       <img
         className="landingMockup"
-        src={LambdaLandingUX}
+        src={uxPlaceholder}
         alt="lambda landing page mockup"
       />
       <div className="coming-soon">COMING SOON!</div>
