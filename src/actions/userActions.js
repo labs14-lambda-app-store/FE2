27c1a237ea6848fbe887d134d181a142db8858b1
Jeff Lapp <lappjeff20@gmail.com>
2019-08-19 16:35:36 -0400

    Fixed loginUser function running on page load when no cookie exists

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -5,13 +5,15 @@ export const LOGIN_USER_START = "LOGIN_USER_START"
 export const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS"
 export const LOGIN_USER_FAIL = "LOGIN_USER_FAIL"
 
+// checks for user_id cookie set on successful login and returns or creates a user based off of it
 export const loginUser = user => async dispatch => {
   dispatch({ type: LOGIN_USER_START })
+  //check for cookie
   const userID = Cookie.get("user_id")
 
   try {
     let result = null
-    //if userID stored in cookie, get user with ID in database
+    //if cookie with userID exists, get user with ID in database
     if (userID) {
       result = await axios.get(`${baseUsersUrl}/${userID}`)
       dispatch({
@@ -22,7 +24,8 @@ export const loginUser = user => async dispatch => {
           },
         },
       })
-    } else {
+      //checks for user object to ensure block only runs for user creation
+    } else if (user) {
       //post request returns user data if user is already in DB or creates user then returns user
       result = await axios.post(`${baseUsersUrl}`, user)
       //sets user_id cookie when user logs in - expires in 7 days
