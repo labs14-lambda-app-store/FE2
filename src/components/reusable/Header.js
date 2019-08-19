e6f5fe0c40838747e8b8cb9fe9ac8a2bbf075a92
Jeff Lapp <lappjeff20@gmail.com>
2019-08-19 12:26:48 -0400

    Changed name of png import to be more descriptive. Formatting change

--- src/components/reusable/Header.js
+++ src/components/reusable/Header.js
@@ -1,13 +1,13 @@
-import React from "react";
-import lambdaStudent from '../../assets/lambda-student.png'
+import React from "react"
+import lambdaStudentPic from "../../assets/lambda-student.png"
 
 export default function Header() {
   return (
     <header>
       <div className="home-header">
         <p>Lambda App Store</p>
-        <img src={lambdaStudent} alt='lambda-student-header'></img>
+        <img src={lambdaStudentPic} alt="lambda-student-header"></img>
       </div>
     </header>
-  );
+  )
 }
