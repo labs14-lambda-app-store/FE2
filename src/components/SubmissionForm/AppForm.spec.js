d94790cdac5ce43908db5492d43b340a1d317ed6
Kayla <6lackmacaroon@gmail.com>
2019-08-20 15:34:45 -0600

    reached out to help channel for assistance with this error, made no progress, gotten a few other errors but eventually it all leads back to invarient violation error

--- src/components/SubmissionForm/AppForm.spec.js
+++ src/components/SubmissionForm/AppForm.spec.js
@@ -1,25 +1,30 @@
 import React from "react"
 // import ReactDOM from 'react-dom'
 import "@testing-library/jest-dom"
-import "@testing-library/react/cleanup-after-each"
-import { render, fireEvent } from "@testing-library/react"
+import { render, cleanup } from "@testing-library/react"
 import AppDetails from "./AppDetails"
 import AppForm from "./AppForm"
 // import Confirm from "./Confirm"
 // import Success from "./Success"
 
+afterEach(cleanup)
+
 describe("<AppForm />", () => {
-      // const div = document.createElement('div')
-      it("renders without errors", () => {
-            render(<AppForm />)
-      });
-      // it("should render <AppDetails /> without errors", async () => {
-      //       // await render(<AppDetails />)
-      // });
-      // it("should render <Confirm /> without errors", async () => {
-      //       // await render(<Confirm />)
-      // });
-      // it("should render <Success /> without errors", async () => {
-      //       // await render(<Success />)
-      // })
+  it("renders without errors", () => {
+    render(<AppForm />)
+  })
 })
+
+describe("<AppDetails />", () => {
+  it("renders without errors", () => {
+    const { form } = render(<AppDetails />)
+    expect(form).toHaveClass("submission")
+  })
+})
+
+//   it("should render <Confirm /> without errors", async () => {
+//         // await render(<Confirm />)
+//   });
+//   it("should render <Success /> without errors", async () => {
+//         // await render(<Success />)
+//   })
---
+++ src/components/SubmissionForm/Success.spec.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from 'react-dom';
+import "@testing-library/jest-dom"
+import "@testing-library/react/cleanup-after-each"
+import { render } from "@testing-library/react"
+import Success from "./Success";
+
+describe("<Success />", () => {
+  
+  it("should render the App component without errors", async () => {
+//     await render(<Success />)
+  })
+})
