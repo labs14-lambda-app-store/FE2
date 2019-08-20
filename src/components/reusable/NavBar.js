df3972670ad1e9a53e44f28e4f5fafcf3bb91e0d
victorN <victornguyen082@gmail.com>
2019-08-20 13:43:39 -0400

    Toggle NavBar Component

--- src/components/reusable/NavBar.js
+++ src/components/reusable/NavBar.js
@@ -7,74 +7,78 @@ import AuthButton from "../Auth/AuthButton"
 import { MemoryRouter as Router, withRouter } from "react-router-dom"
 import redLambdaLogo from "../../assets/Lambda_Logo.png"
 
-const NavBar = ({ user, history }) => {
-  return (
-    <Router>
-      <div>
-        <AppBar position="static">
-          <Toolbar>
-            <img
-              className="Header-lambda-logo"
-              src={redLambdaLogo}
-              alt="red lambda logo"
-              onClick={() => history.push("/")}
-            />
-
-            {/* Using button onClick to avoid Link bug that prevents route changes,
-                        when using material-ui */}
-            <div className="buttons">
-              <Button onClick={() => history.push("/")}>Home</Button>
-              <Button
-                onClick={() => {
-                  history.push("/apps")
-                }}
-              >
-                Apps
-              </Button>
-
-              {/* if there is a user, and the user's roll is admin, show pending apps nav button, else if there is a user and the user's role isn't admin, show the submit form nav button. else don't show either buttons */}
-              {user ? (
-                user.role === "admin" ? (
-                  <Button
-                    className="pendingAppsButton"
-                    onClick={() => history.push("/pending-apps")}
-                  >
-                    Pending Apps
-                  </Button>
-                ) : (
+const NavBar = ({ user, history, location }) => {
+  if (location.pathname === "/dashboard") {
+    return null;
+  } else {
+    return (
+      <Router>
+        <div>
+          <AppBar position="static">
+            <Toolbar>
+              <img
+                className="Header-lambda-logo"
+                src={redLambdaLogo}
+                alt="red lambda logo"
+                onClick={() => history.push("/")}
+              />
+  
+              {/* Using button onClick to avoid Link bug that prevents route changes,
+                          when using material-ui */}
+              <div className="buttons">
+                <Button onClick={() => history.push("/")}>Home</Button>
+                <Button
+                  onClick={() => {
+                    history.push("/apps")
+                  }}
+                >
+                  Apps
+                </Button>
+  
+                {/* if there is a user, and the user's roll is admin, show pending apps nav button, else if there is a user and the user's role isn't admin, show the submit form nav button. else don't show either buttons */}
+                {user ? (
+                  user.role === "admin" ? (
                     <Button
-                      className="appFormButton"
-                      onClick={() => history.push("/app-form")}
+                      className="pendingAppsButton"
+                      onClick={() => history.push("/pending-apps")}
                     >
-                      Submit App
-                  </Button>
+                      Pending Apps
+                    </Button>
+                  ) : (
+                      <Button
+                        className="appFormButton"
+                        onClick={() => history.push("/app-form")}
+                      >
+                        Submit App
+                    </Button>
+                    )
+                ) : null}
+                {user ? (
+                  user.role === "student" ? (
+                    <Button
+                      className="studentProfile"
+                      onClick={() => history.push("/dashboard")}
+                    >
+                      My Profile
+                    </Button>
+                  ) : user.role === "admin"(
+                    <Button
+                      className="pendingAppsButton"
+                      onClick={() => history.push("/pending-apps")}
+                    >
+                      Pending Apps
+                    </Button>
                   )
-              ) : null}
-              {user ? (
-                user.role === "student" ? (
-                  <Button
-                    className="studentProfile"
-                    onClick={() => history.push("/dashboard")}
-                  >
-                    My Profile
-                  </Button>
-                ) : user.role === "admin"(
-                  <Button
-                    className="pendingAppsButton"
-                    onClick={() => history.push("/pending-apps")}
-                  >
-                    Pending Apps
-                  </Button>
-                )
-              ) : null}
-
-              < AuthButton />
-            </div>
-          </Toolbar>
-        </AppBar>
-      </div>
-    </Router>
-  )
+                ) : null}
+  
+                < AuthButton />
+              </div>
+            </Toolbar>
+          </AppBar>
+        </div>
+      </Router>
+    )
+  }
 }
 
 const mapStateToProps = ({ usersReducer }) => {
