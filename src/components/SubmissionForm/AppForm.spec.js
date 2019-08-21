d3103dc6552d1fb73988646bd1d00c37f70d4ece
Kayla <6lackmacaroon@gmail.com>
2019-08-21 11:02:36 -0600

    handful of working tests THANKS HAMZAgit add .

--- src/components/SubmissionForm/AppForm.spec.js
+++ src/components/SubmissionForm/AppForm.spec.js
@@ -1,30 +1,64 @@
 import React from "react"
-// import ReactDOM from 'react-dom'
+import { Provider } from "react-redux"
+import { store } from "../../utils/store"
+import ReactDOM from "react-dom"
 import "@testing-library/jest-dom"
 import { render, cleanup } from "@testing-library/react"
 import AppDetails from "./AppDetails"
 import AppForm from "./AppForm"
-// import Confirm from "./Confirm"
-// import Success from "./Success"
+import NavBar from '../reusable/NavBar'
+import Confirm from './Confirm'
+import Success from './Success'
 
 afterEach(cleanup)
 
 describe("<AppForm />", () => {
-  it("renders without errors", () => {
-    render(<AppForm />)
+      const app = {
+            "id": 6,
+            "name": "FancyPlants",
+            "is_approved": true,
+            "description": "Poky-man go for flowers! Capture photos of flowers on all your travels, extra points for rare varieties and colors.",
+            "hosted_url": "https://en.wikipedia.org/wiki/Animal_Crossing",
+            "frontend_url": "https://github.com/labs14-lambda-app-store/be2",
+            "backend_url": "https://github.com/labs14-lambda-app-store/FE2",
+            "display_image": "https://source.unsplash.com/700x390/?flowers,wildflowers",
+            "category_id": 4,
+      }
+  it("renders without crashing", () => {
+    render(<Provider store={store}><AppForm app={app}/></Provider>)
   })
+  it("should find h1 title", () => {
+      const {getByText} = render(<Provider store={store}><AppForm app={app}/></Provider>)
+      getByText(/Submit Your App/i)
+  })
+  it("should find input Categories", () => {
+      const {getByText} = render(<Provider store={store}><AppForm app={app}/></Provider>)
+      getByText(/Categories/i)
+  })
+  it("should find input App Name", () => {
+      const {getByText} = render(<Provider store={store}><AppForm app={app}/></Provider>)
+      getByText(/App Name/i)
+  })
+  it("should find input Description", () => {
+      const {getByText} = render(<Provider store={store}><AppForm app={app}/></Provider>)
+      getByText(/Description/i)
+  })
+  it("should find input Hosted Url", () => {
+      const {getByText} = render(<Provider store={store}><AppForm app={app}/></Provider>)
+      getByText(/Hosted URL/i)
+  })
+  it("should find input frontend Url", () => {
+      const {getByText} = render(<Provider store={store}><AppForm app={app}/></Provider>)
+      getByText(/Frontend URL/i)
+  })
+  it("should find input backend Url", () => {
+      const {getByText} = render(<Provider store={store}><AppForm app={app}/></Provider>)
+      getByText(/Backend URL/i)
+  })
+  it("should find button Continue", () => {
+      const {getByText} = render(<Provider store={store}><AppForm app={app}/></Provider>)
+      getByText(/Continue/i)
+  })
+
 })
 
-describe("<AppDetails />", () => {
-  it("renders without errors", () => {
-    const { form } = render(<AppDetails />)
-    expect(form).toHaveClass("submission")
-  })
-})
-
-//   it("should render <Confirm /> without errors", async () => {
-//         // await render(<Confirm />)
-//   });
-//   it("should render <Success /> without errors", async () => {
-//         // await render(<Success />)
-//   })
--- src/components/SubmissionForm/Success.spec.js
+++ src/components/SubmissionForm/Success.spec.js
@@ -1,13 +1,24 @@
 import React from "react"
-import ReactDOM from 'react-dom';
-import "@testing-library/jest-dom"
-import "@testing-library/react/cleanup-after-each"
+import { Provider } from "react-redux"
+import { store } from "../../utils/store"
 import { render } from "@testing-library/react"
-import Success from "./Success";
+import Success from './Success'
+
 
 describe("<Success />", () => {
+  const app = {
+    "id": 6,
+    "name": "FancyPlants",
+    "is_approved": true,
+    "description": "Poky-man go for flowers! Capture photos of flowers on all your travels, extra points for rare varieties and colors.",
+    "hosted_url": "https://en.wikipedia.org/wiki/Animal_Crossing",
+    "frontend_url": "https://github.com/labs14-lambda-app-store/be2",
+    "backend_url": "https://github.com/labs14-lambda-app-store/FE2",
+    "display_image": "https://source.unsplash.com/700x390/?flowers,wildflowers",
+    "category_id": 4,
+  }
   
-  it("should render the App component without errors", async () => {
-//     await render(<Success />)
+  it("should render the App component without errors",  () => {
+    // render(<Provider store={store}><Success app={app} /></Provider>)
   })
 })
