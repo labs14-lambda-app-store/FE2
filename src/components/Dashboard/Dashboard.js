8d79c10146fef09b6689f5fa6f8f57f9f1a5a0a7
Jeff Lapp <lappjeff20@gmail.com>
2019-08-22 13:09:35 -0400

    Set up basic routes for Portfolio view and created skeleton

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,94 +1,106 @@
-import React from 'react';
-import ProtectedRoute from "../Auth/ProtectedRoute";
-import Profile from "./Profile";
-import Overview from "./Overview";
-import { connect } from "react-redux";
+import React from "react"
+import ProtectedRoute from "../Auth/ProtectedRoute"
+import Profile from "./Profile"
+import Overview from "./Overview"
+import { connect } from "react-redux"
 import useStyles from "./styles"
-import clsx from 'clsx';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Container from '@material-ui/core/Container';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import List from '@material-ui/core/List';
-import MainItemList from './MainItemList';
-import SecondaryItemList from './SecondaryItemList'
+import clsx from "clsx"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import Drawer from "@material-ui/core/Drawer"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import Divider from "@material-ui/core/Divider"
+import IconButton from "@material-ui/core/IconButton"
+import Container from "@material-ui/core/Container"
+import MenuIcon from "@material-ui/icons/Menu"
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft"
+import List from "@material-ui/core/List"
+import MainItemList from "./MainItemList"
+import SecondaryItemList from "./SecondaryItemList"
+import Portfolio from "./Portfolio"
 
 const Dashboard = ({ user }) => {
-
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(true);
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-                <Toolbar className={classes.toolbar}>
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography component="h1" variant="h4" noWrap className={classes.title}>
-                        {user.first_name ? `${user.first_name}'s` : "User"} Dashboard
-             </Typography>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                variant="permanent"
-                classes={{
-                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-                }}
-                open={open}
-            >
-                <div className={classes.toolbarIcon}>
-                    <IconButton onClick={handleDrawerClose}>
-                        <ChevronLeftIcon />
-                    </IconButton>
-                </div>
-                <Divider />
-
-                <List className={classes.list}>
-                    <MainItemList />
-                </List>
-                <Divider />
-                <List className={classes.list}>
-                    <SecondaryItemList />
-                </List>
-            </Drawer>
-
-            <main className={classes.content}>
-                <div className={classes.appBarSpacer} />
-                <Container maxWidth="lg" className={classes.container}>
-                    <ProtectedRoute exact path="/dashboard" component={Overview} />
-                    <ProtectedRoute path="/dashboard/profile" component={Profile} />
-                </Container>
-            </main>
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(true)
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden
+            )}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h4"
+            noWrap
+            className={classes.title}
+          >
+            {user.first_name ? `${user.first_name}'s` : "User"} Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
         </div>
-    );
+        <Divider />
+
+        <List className={classes.list}>
+          <MainItemList />
+        </List>
+        <Divider />
+        <List className={classes.list}>
+          <SecondaryItemList />
+        </List>
+      </Drawer>
+
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <ProtectedRoute exact path="/dashboard" component={Overview} />
+          <ProtectedRoute path="/dashboard/profile" component={Profile} />
+          <ProtectedRoute path="/dashboard/Portfolio" component={Portfolio} />
+        </Container>
+      </main>
+    </div>
+  )
 }
 
 const mapStateToProps = ({ usersReducer }) => {
-    return {
-        ...usersReducer,
-    }
+  return {
+    ...usersReducer,
+  }
 }
 
 export default connect(
-    mapStateToProps,
-    {}
+  mapStateToProps,
+  {}
 )(Dashboard)
--- src/components/Dashboard/MainItemList.js
+++ src/components/Dashboard/MainItemList.js
@@ -1,20 +1,36 @@
-import React from 'react';
-import { withRouter } from 'react-router';
-import SideNavItem from '../reusable/SideNavItem';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import PersonIcon from '@material-ui/icons/Person';
-import AddBoxIcon from '@material-ui/icons/AddBox';
-import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import React from "react"
+import { withRouter } from "react-router"
+import SideNavItem from "../reusable/SideNavItem"
+import DashboardIcon from "@material-ui/icons/Dashboard"
+import PersonIcon from "@material-ui/icons/Person"
+import AddBoxIcon from "@material-ui/icons/AddBox"
+import CheckBoxIcon from "@material-ui/icons/CheckBox"
 
 const MainItemList = () => {
-    return (
-        <div>
-            <SideNavItem route="/dashboard" icon={() => <DashboardIcon />} primaryText="Overview" />
-            <SideNavItem route="/dashboard/approved-apps" icon={() => <CheckBoxIcon />} primaryText="My Approved Apps" />
-            <SideNavItem route="/app-form" icon={() => <AddBoxIcon />} primaryText="Submit an App" />
-            <SideNavItem route="/dashboard/profile" icon={() => <PersonIcon />} primaryText="My Profile" />
-        </div>
-    )
+  return (
+    <div>
+      <SideNavItem
+        route="/dashboard"
+        icon={() => <DashboardIcon />}
+        primaryText="Overview"
+      />
+      <SideNavItem
+        route="/dashboard/portfolio"
+        icon={() => <CheckBoxIcon />}
+        primaryText="My Apps"
+      />
+      <SideNavItem
+        route="/app-form"
+        icon={() => <AddBoxIcon />}
+        primaryText="Submit an App"
+      />
+      <SideNavItem
+        route="/dashboard/profile"
+        icon={() => <PersonIcon />}
+        primaryText="My Profile"
+      />
+    </div>
+  )
 }
 
 export default withRouter(MainItemList)
