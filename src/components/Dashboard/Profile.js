0cb317fc4f26ca8d27f23b7e16816db00faf1f69
Kayla <6lackmacaroon@gmail.com>
2019-08-22 12:08:53 -0600

    add basic styling - pulling props but idk why its victors...

--- src/components/Dashboard/Profile.js
+++ src/components/Dashboard/Profile.js
@@ -1,27 +1,42 @@
-import React from 'react';
-
+import React from 'react'
+import { connect } from 'react-redux'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
 
 
 const Profile = ({ user }) => {
+
+    
+
     return (
         <div className="profileContainer">
-            <div className="infoCard">
-                <div className="infoCard">
-                    <name>First & Last</name>
-                    <email>email@email.com</email>
-                    <pronoun>of preference!</pronoun>
+            <Paper className="infoCard">
+                <div className="nameCard">
+                    <name>{user.first_name} {user.last_name}</name>
+                    <email>{user.email}</email>
+                    <birthday>user dot birthday</birthday>
+                    <timezone>user dot time zone</timezone>
+                    <pronoun>pronoun of preference!</pronoun>
                 </div>
                 <div className="buttonCard">
-                    <button className="editProfileButton">edit</button>
+                    <Button color="primary" className="editProfileButton">edit profile</Button>
                 </div>
-            </div>
-            <div className="imageCard">
-                <image></image>
-                <username></username>
-            </div>
+            </Paper>
+            <Paper className="imageCard">
+                <div className="imgDiv"><img src={user.pictureURL} className="profileImage" alt="user's face"/></div>
+                <div className="usernameDiv"><username>{user.username}</username></div>
+            </Paper>
         </div>
     )
 }
 
+const mapStateToProps = ({ usersReducer }) => {
+    return {
+        ...usersReducer
+    }
+}
 
-export default Profile;
+export default connect(
+    mapStateToProps,
+    {}
+)(Profile);
