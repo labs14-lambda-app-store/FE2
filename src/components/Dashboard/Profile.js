04109cb54dba2bbac436d53eef1e76115d55f64e
Kayla <6lackmacaroon@gmail.com>
2019-08-22 10:57:43 -0600

    add basic skeletop layout for profile view

--- src/components/Dashboard/Profile.js
+++ src/components/Dashboard/Profile.js
@@ -4,8 +4,21 @@ import React from 'react';
 
 const Profile = ({ user }) => {
     return (
-        <div className="profile-container">
-
+        <div className="profileContainer">
+            <div className="infoCard">
+                <div className="infoCard">
+                    <name>First & Last</name>
+                    <email>email@email.com</email>
+                    <pronoun>of preference!</pronoun>
+                </div>
+                <div className="buttonCard">
+                    <button className="editProfileButton">edit</button>
+                </div>
+            </div>
+            <div className="imageCard">
+                <image></image>
+                <username></username>
+            </div>
         </div>
     )
 }
