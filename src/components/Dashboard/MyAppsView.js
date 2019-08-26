832469b94f8abbe592b9995a80c03b2fbc8bc7af
Kayla <6lackmacaroon@gmail.com>
2019-08-26 15:45:56 -0600

    fixed bug when no apps are available to show

--- src/components/Dashboard/MyAppsView.js
+++ src/components/Dashboard/MyAppsView.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from "react-redux";
 
 import Paper from '@material-ui/core/Paper';
@@ -11,28 +11,34 @@ const MyAppsView = ({ user }) => {
     const classes = useStyles();
     const fixedAppsHeightPaper = clsx(classes.paper, classes.fixedAppHeight);
     const [isOpen, setIsOpen] = useState(false)
-    const approvedApps = user.apps.filter(app => app.is_approved);
-    const unapprovedApps = user.apps.filter(app => app.is_approved === false);
+    const [approvedApps, setApprovedApps] = useState("")
+    const [unapprovedApps, setUnapprovedApps] = useState("")
+
+    useEffect(() => {
+        setApprovedApps(user.apps.filter(app => app.is_approved))
+        setUnapprovedApps(user.apps.filter(app => app.is_approved === false))
+    }, [])
+
+    const checkArrayLength = array => {
+        if(array.length > 0){
+            return <div onClick={() => setIsOpen(true)} style={{ display: "flex", flexDirection: "column" }}>
+            {array.name}
+            <img src={array.display_image} style={{ width: 500, height: "auto" }} />
+        </div>
+        } 
+        return <p>no apps to show</p>
+    }
+
 
     return (
         <main className="my-apps-container">
             <Paper className="unapproved-apps-card">
-                <h2>Pending Apps</h2>
-                {unapprovedApps.map(app => (
-                    <div onClick={() => setIsOpen(true)} style={{ display: "flex", flexDirection: "column" }}>
-                        {app.name}
-                        <img src={app.display_image} style={{ width: 500, height: "auto" }} />
-                    </div>
-                ))}
+            <h2>Unapproved Apps</h2>
+                {checkArrayLength(unapprovedApps) }
             </Paper>
             <Paper className="approved-apps-card">
                 <h2>Approved Apps</h2>
-                {approvedApps.map(app => (
-                    <div onClick={() => setIsOpen(true)} style={{ display: "flex", flexDirection: "column" }}>
-                        {app.name}
-                        <img src={app.display_image} style={{ width: 500, height: "auto" }} />
-                    </div>
-                ))}
+                {checkArrayLength(approvedApps)}
             </Paper>
         </main>
     )
