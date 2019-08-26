8ec6dad879fce737b83da336f27e46b5321f215f
joshua-disney <joshua.a.disney@gmail.com>
2019-08-26 12:32:48 -0700

    Flesh out basic layout of appPage

--- src/components/AppPage/AppPage.js
+++ src/components/AppPage/AppPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react"
+import { Container } from "@material-ui/core"
 
 const AppPage = props => {
   const {
@@ -14,9 +15,45 @@ const AppPage = props => {
   } = props
 
   return (
-    <div>
-      <h1>This is an App page</h1>
-    </div>
+    <Container>
+      <div className="title-card">
+        <h2>App name!</h2>
+        <h2>{name}</h2>
+        <div>
+          <p>here there be images</p>
+          <img src={display_image} alt="A[[ screenshot" />
+        </div>
+      </div>
+
+      <div>
+        <h3>this is a category</h3>
+        <h3>{category}</h3>
+        <br />
+        <p>This is where the description goes.</p>
+        <p>{description}</p>
+      </div>
+
+      <div>
+        {tags.map(tag => {
+          ;<p>tag name</p>
+        })}
+      </div>
+
+      <div className="url-section">
+        <h3>hosted_url</h3>
+        <h3>{hosted_url}</h3>
+        <h3>frontend_url</h3>
+        <h3>{frontend_url}</h3>
+        <h3>backend_url</h3>
+        <h3>{backend_url}</h3>
+      </div>
+
+      <div>
+        {users.map(user => {
+          ;<div>userDiv</div>
+        })}
+      </div>
+    </Container>
   )
 }
 
