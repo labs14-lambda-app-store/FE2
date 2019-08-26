2dd7b7c69e5aaae5956b1b2d15adefee421ebbcb
Kayla <6lackmacaroon@gmail.com>
2019-08-26 13:23:13 -0600

    bring submit and deny buttons from profile-edit

--- src/components/Dashboard/AppContent.js
+++ src/components/Dashboard/AppContent.js
@@ -1,37 +1,81 @@
 import React from "react"
 import { connect } from "react-redux"
-import Paper from "@material-ui/core/Paper"
 import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
 import { updateApp } from "../../actions"
 
 const AppContent = ({ user, app }) => {
   const { id } = user
-  const { display_image, name, description, frontend_url, backend_url, hosted_url, category, is_approved } = app
+  const {
+    display_image,
+    name,
+    description,
+    frontend_url,
+    backend_url,
+    hosted_url,
+    category,
+    is_approved,
+  } = app
 
   const [updatedApp, setUpdatedApp] = useState({
     display_image,
     name,
     description,
-    frontend_url, 
-    backend_url, 
+    frontend_url,
+    backend_url,
     hosted_url,
     category,
-    is_approved,
   })
+
   function handleUpdateApp(change, id) {
     updateApp(change, id).then(res => {
       window.location.reload()
     })
   }
 
-  function handleChanges(e) {}
+  const handleChanges = e => {
+    setUpdatedApp({ ...updatedApp, [e.target.name]: e.target.value })
+  }
 
   return (
-    <>
-      <h1>{name}</h1>
-      <p>{description}</p>
-    </>
+    <main className="app-edit-content">
+      <div className="app-edit-form">
+        <TextField
+          id="outlined-with-placeholder"
+          margin="normal"
+          variant="outlined"
+          value={updatedApp.name}
+          name="name"
+          label="name"
+          onChange={e => {
+            handleChanges(e)
+          }}
+        />
+        <p>{description}</p>
+      </div>
+      <div className="app-edit-buttons">
+        <Tooltip title="Approve" placement="top">
+          <Button
+            className="app-edit-button"
+            size="small"
+            color="primary"
+            onClick={() => handleUpdateApp(updatedApp, id)}
+          >
+            <i class="fas fa-check-circle fa-2x"></i>
+          </Button>
+        </Tooltip>
+        <Tooltip title="Approve" placement="top">
+          <Button
+            className="app-edit-button"
+            size="small"
+            color="primary"
+            onClick={() => handleUpdateApp(updatedApp, id)}
+          >
+            <i class="fas fa-check-circle fa-2x"></i>
+          </Button>
+        </Tooltip>
+      </div>
+    </main>
   )
 }
 
