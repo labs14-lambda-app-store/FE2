253b6c485208e2c5d72de24c17ed619b68640a7c
chrispetito <christopherpetito@gmail.com>
2019-08-26 15:28:09 -0700

    add button selector function, onClick handler to nav bar buttons

--- src/components/reusable/NavBar.js
+++ src/components/reusable/NavBar.js
@@ -7,10 +7,24 @@ import AuthButton from "../Auth/AuthButton"
 import { withRouter } from "react-router-dom"
 import redLambdaLogo from "../../assets/Lambda_Logo.png"
 
-const NavBar = (props) => {
+const buttonSelector = () => {
+  if (window.location.pathname === "/") {
+    return "home-button"
+  } else if (window.location.pathname === "/apps") {
+    return "apps-button"
+  } else if (window.location.pathname === "/pending-apps") {
+    return "pending-apps-button"
+  } else if (window.location.pathname === '/submit-app') {
+    return 'submit-button'
+  }
+}
+
+const NavBar = props => {
+  const [activeButton, setActiveButton] = React.useState(buttonSelector())
+
   const { user, history, location } = props
   if (location.pathname.includes("/dashboard")) {
-    return null;
+    return null
   } else {
     return (
       <div>
@@ -26,35 +40,60 @@ const NavBar = (props) => {
             {/* Using button onClick to avoid Link bug that prevents route changes,
                           when using material-ui */}
             <div className="buttons">
-              <Button onClick={() => history.push("/")}>Home</Button>
               <Button
                 onClick={() => {
+                  setActiveButton("home-button")
+                  history.push("/")
+                }}
+                style={{
+                  color: activeButton === "home-button" ? "#3ab5e5" : "black",
+                }}
+              >
+                Home
+              </Button>
+              <Button
+                onClick={() => {
+                  setActiveButton("apps-button")
                   history.push("/apps")
+                }}
+                style={{
+                  color: activeButton === "apps-button" ? "#3ab5e5" : "black",
                 }}
               >
                 Apps
-                </Button>
+              </Button>
 
               {/* if there is a user, and the user's roll is admin, show pending apps nav button, else if there is a user and the user's role isn't admin, show the submit form nav button. else don't show either buttons */}
               {user ? (
                 user.role === "admin" ? (
                   <Button
-                    className="pendingAppsButton"
-                    onClick={() => history.push("/pending-apps")}
+                    onClick={() => {
+                      setActiveButton("pending-apps-button")
+                      history.push("/pending-apps")
+                    }}
+                    style={{
+                      color:
+                        activeButton === "pending-apps-button"
+                          ? "#3ab5e5"
+                          : "black",
+                    }}
                   >
                     Pending Apps
-                    </Button>
+                  </Button>
                 ) : (
-                    <Button
-                      className="appFormButton"
-                      onClick={() => history.push("/dashboard/submit-app")}
-                    >
-                      Submit App
-                    </Button>
-                  )
+                  <Button
+                    className="appFormButton"
+                    onClick={() => {
+                      setActiveButton('submit-button')
+                      history.push("/dashboard/submit-app")
+                    }}
+                  >
+                    Submit App
+                  </Button>
+                )
               ) : null}
 
-              < AuthButton />
+              <AuthButton />
             </div>
           </Toolbar>
         </AppBar>
