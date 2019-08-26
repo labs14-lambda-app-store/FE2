a07f882a6aa6dd061181e4e5d9f2578cb04ab415
victorN <victornguyen082@gmail.com>
2019-08-26 13:51:56 -0400

    changes to drowndown and dashboard navigation

--- src/components/Dashboard/MainItemList.js
+++ src/components/Dashboard/MainItemList.js
@@ -9,7 +9,6 @@ const MainItemList = () => {
     return (
         <div>
             <SideNavItem route="/dashboard/apps" icon={() => <CategoryIcon />} primaryText="My Apps" />
-
             <SideNavItem route="/dashboard/profile" icon={() => <PersonIcon />} primaryText="My Profile" />
         </div>
     )
