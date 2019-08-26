d5de086b845ccd07c5162167ebefc20a5ae794fd
Kayla <6lackmacaroon@gmail.com>
2019-08-26 13:17:20 -0600

    build out functions for handling changes and handling App update

--- src/components/Dashboard/AppContent.js
+++ src/components/Dashboard/AppContent.js
@@ -3,12 +3,29 @@ import { connect } from "react-redux"
 import Paper from "@material-ui/core/Paper"
 import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
+import { updateApp } from "../../actions"
 
 const AppContent = ({ user, app }) => {
   const { id } = user
-  const { display_image, name, description, category, is_approved } = app
+  const { display_image, name, description, frontend_url, backend_url, hosted_url, category, is_approved } = app
 
+  const [updatedApp, setUpdatedApp] = useState({
+    display_image,
+    name,
+    description,
+    frontend_url, 
+    backend_url, 
+    hosted_url,
+    category,
+    is_approved,
+  })
+  function handleUpdateApp(change, id) {
+    updateApp(change, id).then(res => {
+      window.location.reload()
+    })
+  }
 
+  function handleChanges(e) {}
 
   return (
     <>
