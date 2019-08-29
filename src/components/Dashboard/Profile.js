627ba11631dcdf1e50bf29a10765e5d6a0baebd9
chrispetito <christopherpetito@gmail.com>
2019-08-29 10:34:17 -0700

    add styling to profile view

--- src/components/Dashboard/Profile.js
+++ src/components/Dashboard/Profile.js
@@ -7,7 +7,17 @@ import Button from "@material-ui/core/Button"
 import ProfileEditModal from "./ProfileEditModal"
 
 const Profile = ({ user }) => {
-  const { first_name, last_name, email, pictureURL, username, preferred_pronoun, birthday, github_link, linkedin_link } = user
+  const {
+    first_name,
+    last_name,
+    email,
+    pictureURL,
+    username,
+    preferred_pronoun,
+    birthday,
+    github_link,
+    linkedin_link,
+  } = user
   console.log(user)
 
   const [isOpen, setIsOpen] = useState(false)
@@ -19,12 +29,35 @@ const Profile = ({ user }) => {
           <h2>
             {first_name} {last_name}
           </h2>
-          <p><i class="fas fa-genderless" aria-hidden='true'></i>{preferred_pronoun}</p>
-          <p><i class="fas fa-envelope" aria-hidden='true'></i>{email}</p>
-          <p><i class="fas fa-birthday-cake" aria-hidden='true'></i>{birthday}</p>
-          <a href={`${github_link}`} target='_onblank'>View GitHub</a>
-          <p><i class="fab fa-github" aria-hidden='true'></i>{github_link}</p>
-          <p><i class="fab fa-linkedin-in" aria-hidden='true'></i>{linkedin_link}</p>
+          <p className="info-label">
+            <i class="fas fa-genderless" aria-hidden="true"></i>Preferred
+            Pronoun
+          </p>
+          <p className='info-text'>{preferred_pronoun}</p>
+          <p className="info-label">
+            <i class="fas fa-envelope" aria-hidden="true"></i>Email
+          </p>
+          <p className='info-text'>
+            {email}
+          </p>
+          <p className="info-label">
+            <i class="fas fa-birthday-cake" aria-hidden="true"></i>Birthday
+          </p>
+          <p className='info-text'> {birthday}</p>
+          <p className="info-label">
+            <i class="fab fa-github" aria-hidden="true"></i>GitHub
+          </p>
+          <p className='info-text'>{github_link}</p>
+          <a href={`${github_link}`} target="_onblank">
+          <i class="fas fa-external-link-alt"></i>View GitHub
+          </a>
+          <p className="info-label">
+            <i class="fab fa-linkedin-in" aria-hidden="true"></i>LinkedIn
+          </p>
+          <p className='info-text'>{linkedin_link}</p>
+          <a href={`${linkedin_link}`} target="_onblank">
+          <i class="fas fa-external-link-alt"></i>View LinkedIn
+          </a>
         </div>
         <div className="button-card" onClick={() => setIsOpen(true)}>
           <Button size="small" color="primary" className="edit-profile-button">
